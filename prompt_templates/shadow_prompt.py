b74ef1dcd4de923d75889b903fe36ef3fde6c1de
marib <i.maribsultan@gmail.com>
2023-07-01 18:21:43 +0500

    <message_here>

--- prompt_templates/shadow_prompt.py
+++ prompt_templates/shadow_prompt.py
@@ -13,10 +13,12 @@ prompt_template = """Answer the following questions as best you can. You have ac
 Use the following format:
 
 Question: the input question you must answer
+Thought: you should always think about what to do
 Action: the action to take, should be one of [{tool_names}]
-Action Input: the entire original question
+Action Input: part of the question that is relevant for current action
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
 Final Answer: the final answer to the original input question
 
 Begin!
