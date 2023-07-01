c5441320ec8f867592a22b6791d41b9380289b54
marib <i.maribsultan@gmail.com>
2023-07-02 01:00:56 +0500

    <message_here>

--- prompt_templates/shadow_prompt.py
+++ prompt_templates/shadow_prompt.py
@@ -15,7 +15,7 @@ Use the following format:
 Task: the task you must perform
 Thought: you should always think about what to do
 Action: the action to take, should be one of [{tool_names}]
-Action Input: a part of the original instruction, should be from "{input}".
+Action Input: a part of the original instruction
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I have now performed all tasks
