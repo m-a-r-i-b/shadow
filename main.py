1215a0cc68f33512c4102755c3f51aae3d3e304f
marib <i.maribsultan@gmail.com>
2023-07-01 23:52:07 +0500

    <message_here>

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from shadow_agent import ShadowAgent
 
 # prompt = "add a button to decrement count below the increment button. Once done, commit code with appropriate message"
 # prompt = "add a button to decrement count below the increment button. After that stage, commit and push the code in current branch with appropriate commit message"
-prompt = "add a button to decrement count, below the increment button. After that stage, commit and then push the code in current branch with some commit message 'changes done'"
+prompt = "add a button to decrement count, below the increment button. After that stage, commit and then push the code in current branch"
 
 shadow_agent = ShadowAgent()
 shadow_agent.run(prompt)
