6218e1874a8dba8b26ccb8399b81abae4b94549a
marib <i.maribsultan@gmail.com>
2023-07-01 23:46:26 +0500

    <message_here>

--- prompt_templates/shadow_prompt.py
+++ prompt_templates/shadow_prompt.py
@@ -15,11 +15,13 @@ Use the following format:
 Task: the task you must perform
 Thought: you should always think about what to do
 Action: the action to take, should be one of [{tool_names}]
-Action Input: a part of the original instruction, with some context if needed. Should be from "{input}".
+Action Input: a part of the original instruction. Should be from "{input}".
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I have now performed all tasks
 Final Answer: the final answer to the original task
+
+Note : You can add the commit message or branch name if required, based on previous task.
 
 Begin!
 
